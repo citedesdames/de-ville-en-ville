47ca2b52e5a0f8aa47c52d4a564ce4f396b83ced
Wintxer <47366828+Wintxer@users.noreply.github.com>
2021-04-16 17:35:32 +0200

    Add files via upload

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,23 +1,69 @@
+
+// Paramétrage de la vue sur la carte
 var mymap = L.map('mapid').setView([47, 2], 6);
 
+
+// Ajout du fond de carte
 L.tileLayer('http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {
     attribution: '',
     maxZoom: 18,
 }).addTo(mymap); 
 
-/*         var map = L.map('mapid').setView([48.833, 2.333], 7); // LIGNE 18
 
-var osmLayer = L.tileLayer('http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', { // LIGNE 20
-    attribution: '© OpenStreetMap contributors',
-    maxZoom: 19
-});
+function ChangeClass() {
+    var element1 = document.querySelector(".main_container");
+    var element2 = document.getElementById("mapid");
+    var element3 = document.querySelector(".arrow");
+    var element4 = document.querySelector(".arrow-top");
+    var element5 = document.querySelector(".arrow-bottom");
+    var element6 = document.querySelector("header");
+    var element7 = document.querySelector("section");
+    element1.classList.toggle("open");
+    element2.classList.toggle("short");
+    element3.classList.toggle("arrow-open");
+    element4.classList.toggle("arrow-top-open");
+    element5.classList.toggle("arrow-bottom-open");
+    element6.classList.toggle("header-open");
+    element7.classList.toggle("section-open");
 
-map.addLayer(osmLayer); */
+    var el = document.querySelector("header h1");
+    if(el.classList.contains('is-hidden')){
+        fadeIn(el);
+      }
+      else {
+        fadeOut(el);
+        setTimeout(function(){fadeIn(el)}, 1000);
+    }
+}
+
+// Fonction pour fadeOut en JS Vanilla
+function fadeOut(el){
+    el.style.opacity = 1;
+  
+    (function fade() {
+      if ((el.style.opacity -= 1) < 0) {
+        el.style.display = 'none';
+        el.classList.add('is-hidden');
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
+}
 
 
-var polygon = L.polyline([
-    [48.833593730835766, 2.515552671160623],
-    [48, -4],
-    [47, -1],
-    [44.8378, -0.594]
-]).addTo(mymap);
+// Fonction pour fadeIn en JS Vanilla
+function fadeIn(el, display){
+    if (el.classList.contains('is-hidden')){
+      el.classList.remove('is-hidden');
+    }
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+  
+    (function fade() {
+      var val = parseFloat(el.style.opacity);
+      if (!((val += .1) > 1)) {
+        el.style.opacity = val;
+        requestAnimationFrame(fade);
+      }
+    })();
+  }
