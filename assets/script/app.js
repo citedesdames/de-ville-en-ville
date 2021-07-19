80a1d1cc3b9db5b9716bb8a9fda8004db3624ff7
Wintxer <47366828+Wintxer@users.noreply.github.com>
2021-07-19 10:20:38 +0200

    date

--- assets/script/app.js
+++ assets/script/app.js
@@ -171,10 +171,12 @@ fetch('./itineraires.json')
 // Appel de la fonction avec les données en entrées
 setTimeout(function() {
   createMarkers(dataintro[0], dataetape[0], datadocs[0]);
+  let load = document.querySelector(".loader");
+  load.classList.add("load-open");
   if(typeof(etape) == 'number') {
     document.querySelectorAll(".leaflet-marker-icon")[etape].click();
     document.getElementById("popup"+dataetape[0][etape].id_etape).click();
-  } 
+  }
 }, 2500);
 
 // Initialisation des marqueurs
@@ -368,7 +370,11 @@ function createMarkers(dataintro, dataetape, datadocs) {
           vignette = datadocs[n].miniature;
           cardcontent.unshift('<div class="card-header"><img class="card-minia" src="'+vignette+'" alt""><h2 class="card-title">'+dataetape[i].lieu+'</h2></div>');
           // Création du pop up du marqueur sur la carte
-          test.unshift(mark.bindPopup('<div class="popup-wrapper"><div class="vignette" style="background-image: url('+vignette+');">'+dataetape[i].lieu+'</div><div class="popup-container"><p class="date">'+dataetape[i].date+'</p><p id="popup'+dataetape[i].id_etape+'" class="more" data-latlng="'+dataetape[i].latitude+', '+dataetape[i].longitude+'" onclick="onPopup(this)">En savoir plus</p></div></div>', {offset: new L.Point(0, -45)}));
+          if(dataetape[i].date != "?") {
+            test.unshift(mark.bindPopup('<div class="popup-wrapper"><div class="vignette" style="background-image: url('+vignette+');">'+dataetape[i].lieu+'</div><div class="popup-container"><p class="date">'+dataetape[i].date+'</p><p id="popup'+dataetape[i].id_etape+'" class="more" data-latlng="'+dataetape[i].latitude+', '+dataetape[i].longitude+'" onclick="onPopup(this)">En savoir plus</p></div></div>', {offset: new L.Point(0, -45)}));}
+          else {
+            test.unshift(mark.bindPopup('<div class="popup-wrapper"><div class="vignette" style="background-image: url('+vignette+');">'+dataetape[i].lieu+'</div><div class="popup-container"><p class="date"></p><p id="popup'+dataetape[i].id_etape+'" class="more" data-latlng="'+dataetape[i].latitude+', '+dataetape[i].longitude+'" onclick="onPopup(this)">En savoir plus</p></div></div>', {offset: new L.Point(0, -45)}));
+          }
         }
         else {
           // Push dans le tableau une ligne par défaut
@@ -494,7 +500,11 @@ function createMarkers(dataintro, dataetape, datadocs) {
       else {
         // Push au début du tableau la ligne qu'il faudra mettre dans l'html
         cardcontent.push('<div class="card-header"><img class="card-minia" src="'+vignette+'" alt""><h2 class="card-title">'+dataetape[i].lieu+'</h2></div>');
-        test.push(mark.bindPopup('<div class="popup-wrapper"><div class="vignette" style="background-image: url('+vignette+');">'+dataetape[i].lieu+'</div><div class="popup-container"><p class="date">'+dataetape[i].date+'</p><p id="popup'+dataetape[i].id_etape+'" class="more" data-latlng="'+dataetape[i].latitude+', '+dataetape[i].longitude+'" onclick="onPopup(this)">En savoir plus</p></div></div>', {offset: new L.Point(0, -45)}));
+        if(dataetape[i].date != "?") {
+          test.push(mark.bindPopup('<div class="popup-wrapper"><div class="vignette" style="background-image: url('+vignette+');">'+dataetape[i].lieu+'</div><div class="popup-container"><p class="date">'+dataetape[i].date+'</p><p id="popup'+dataetape[i].id_etape+'" class="more" data-latlng="'+dataetape[i].latitude+', '+dataetape[i].longitude+'" onclick="onPopup(this)">En savoir plus</p></div></div>', {offset: new L.Point(0, -45)}));}
+        else {
+          test.push(mark.bindPopup('<div class="popup-wrapper"><div class="vignette" style="background-image: url('+vignette+');">'+dataetape[i].lieu+'</div><div class="popup-container"><p class="date"></p><p id="popup'+dataetape[i].id_etape+'" class="more" data-latlng="'+dataetape[i].latitude+', '+dataetape[i].longitude+'" onclick="onPopup(this)">En savoir plus</p></div></div>', {offset: new L.Point(0, -45)}));
+        }
       }
            
     }
