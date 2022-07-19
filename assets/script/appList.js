d5b746ea8745a160d4fe3654da4865e90e59e0f2
PhilippeGambette <philippe.gambette@laposte.net>
2022-07-19 08:18:27 +0200

    :bento: (new page) page displaying the whole content of the website

---
+++ assets/script/appList.js
@@ -0,0 +1,446 @@
+/*--------------------- Paramétrage de la carte ---------------------*/
+/*--------------------- Paramétrage de la carte ---------------------*/
+/*--------------------- Paramétrage de la carte ---------------------*/
+
+
+// Récupération de l'URL au chargement de la page
+let url = window.location.href.split('?');
+let url2;
+let etape;
+let parametresUrl;
+
+// Ajout de paramètres dans l'URL s'il n'y en a pas
+if(typeof url[1] == "undefined") {
+  window.history.pushState({site: 0}, '', "?site=0");
+  url2 = window.location.href.split('?');
+  etape = undefined;
+  
+  parametresUrl = {
+    "site": url2[1].split("=")[1],
+    "etape": etape
+  }
+}
+// Récupération des paramètres de l'URL s'il y en a
+else {
+  url = url[1].split("&");
+  if(url.length>1) {
+    etape = url[1].split("=")[1];
+    etape = parseInt(etape) - 1;
+    document.querySelector(".img-header").classList.remove("img-header-hidden");
+  }
+  else {
+    etape = undefined;
+  }
+  parametresUrl = {
+    "site": url[0].split("=")[1],
+    "etape": etape
+  }
+}
+
+// Tableaux qui vont contenir les données des .csv
+
+let dataintro = [];
+let dataetape = [];
+let datatexts = [];
+let datamedia = [];
+
+let DocParEtape = {}
+
+// Création des tableaux qui vont stocker les contenus
+let cardcontent = [];
+let carddoc = [];
+let test = [];
+let vignette = "assets/thumbnails/thumbnail-default.jpg";
+
+/* -------------------------- IMPORT DES DONNEES -------------------------- */
+/* -------------------------- IMPORT DES DONNEES -------------------------- */
+/* -------------------------- IMPORT DES DONNEES -------------------------- */
+
+// IMPORT DU FICHIER JSON
+fetch('./itineraires.json')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    // UTILISATION DES DONNEES DU FICHIERS JSON
+
+    // Appel des .csv et push dans les tableaux correspondants
+      Papa.parse(data[parametresUrl.site].proprietes, {
+        download: true,
+        header: true,
+        complete: function (results) {
+          dataintro.push(results.data);
+          Papa.parse(data[parametresUrl.site].etapes, {
+            download: true,
+            header: true,
+            complete: function (results) {
+              dataetape.push(results.data);
+              Papa.parse(data[parametresUrl.site].textes, {
+                download: true,
+                header: true,
+                complete: function (results) {
+                  datatexts.push(results.data);
+                  Papa.parse(data[parametresUrl.site].multimedia, {
+                    download: true,
+                    header: true,
+                    complete: function (results) {
+                      datamedia.push(results.data);
+                      
+                      // Initialisation de l'itinéraire
+                      document.querySelector("title").textContent = dataintro[0][0].title;
+                      document.querySelector("h1").innerHTML += dataintro[0][0].titre_court;
+                      document.querySelector("head").insertAdjacentHTML("beforeend",'<link rel="icon" type="image/png" href="'+dataintro[0][0].favicon+'" />');
+
+                      // Création des marqueurs
+                      Start();
+                      // Appel des textes des itinéraires
+                      file_get_contents(dataintro[0][0].url_texte_code, text_treatment, dataintro[0][0].class_texte);
+                      if(parametresUrl.etape==undefined){
+                         ChangeClass();
+                      }
+                    }
+                  })
+                }
+              })
+            }
+          })
+        }
+      })
+});
+
+// Fonction avec les données en entrées
+function Start() {
+  createMarkers(dataintro[0], dataetape[0], datatexts[0], datamedia[0]);
+}
+
+// Fonction qui effectue l'appel
+function file_get_contents(uri, callback, class_text) {
+  fetch(uri)
+  .then(res => res.text())
+  .then(text=> callback(text, class_text));
+}
+
+// Fonction qui change la string obtenue en élément HTML -> page HTML dans une variable
+function text_treatment(str, class_text) {
+  let parser = new DOMParser();
+	let doc = parser.parseFromString(str, 'text/html');
+  let container = document.querySelector(".overflow-texte");
+  container.appendChild(doc.querySelector(class_text));
+  //console.log(doc.querySelector(class_text));
+}
+
+// Création de la liste des auteurs
+function nomAuteur(insere_url, auteur1_prenom, auteur1_nom, auteur1_url, auteur2_prenom, auteur2_nom, auteur_autres){
+  let chaineAuteur = "";
+  
+  // Ajout de l'auteur1
+  if(auteur1_prenom.length>0){
+     chaineAuteur += auteur1_prenom+" ";
+  }
+  chaineAuteur += auteur1_nom;
+  
+  // Ajout de l'URL pour auteur1 si elle existe et qu'on veut l'insérer
+  if((auteur1_url.length>0) && (insere_url)){
+     chaineAuteur = '<a href="'+auteur1_url+'">'+chaineAuteur+'</a>';
+  }
+  
+  // Ajout de l'auteur2
+  if(auteur2_prenom.length>0 || auteur2_nom.length>0){
+     chaineAuteur += ", ";
+  }
+  if(auteur2_prenom.length>0){
+     chaineAuteur += auteur2_prenom+" ";
+  }
+  chaineAuteur += auteur2_nom
+  
+  // Ajout des autres auteurs
+  if(auteur_autres.length>0){
+     chaineAuteur += ", ";
+  }
+  chaineAuteur += auteur_autres;
+  
+  return chaineAuteur.replace("' ","'").replace("’ ","'");
+}
+
+// Fonction pour ajouter un document
+function addDocument(doc){
+// DOCUMENTS
+console.log(doc);
+        // Documents avec image
+        if(doc.type == "image" || doc.type == "vidéo" || doc.type == "iframe") {
+          let codeHTML = "";
+          let auteur = "";
+          let auteurSansLien = "";
+          codeHTML = nomAuteur(true, doc.auteur_prenom, doc.auteur_nom, doc.auteur_url, "", "", doc.auteur_autres);
+          auteurSansLien = nomAuteur(false, doc.auteur_prenom, doc.auteur_nom, doc.auteur_url, "", "", doc.auteur_autres);
+          if(codeHTML.length>0){
+             auteurSansLien += ", ";
+             codeHTML += ", ";
+          }
+          auteur = codeHTML;
+
+          if(doc.url_document.length>0){
+             codeHTML += '<a href="' + doc.url_document + '">' + doc.titre_document + '</a>';
+          }else{
+             codeHTML += doc.titre_document;
+          }
+          
+          if(doc.legende.length>0){
+             codeHTML += ', '+doc.legende;
+          }
+          
+          if(doc.copyright.length>0){
+             codeHTML += ', '+doc.copyright;
+          }
+
+          let url_image = doc.url_document;
+          if(doc.miniature.length > 0){
+             url_image = doc.miniature;
+          }
+          
+          let icon = "image";
+          
+          // Document is a video
+          if(doc.type == "vidéo"){
+             icon = "video";
+             if(url_image.replace("youtu.be/","").length < url_image.length || url_image.replace("youtube.com/","").length < url_image.length){
+                // Url de vidéo Youtube
+                url_image = '<iframe width="500" height="315" src="' + url_image.replace("www.youtube.com/watch?v=","www.youtube.com/embed/").replace("youtu.be/","www.youtube.com/embed/") + '?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+             } else {
+                url_image = "";
+             }
+             codeHTML += '<br>'+url_image;
+          }
+          
+          // Document is an iframe
+          if(doc.type == "iframe"){
+             icon = "video";
+             codeHTML += '<br>'+url_image;
+          }
+
+          // Document is an image
+          if(doc.type == "image" && url_image.length > 0){
+             if(doc.url_document != undefined){
+                codeHTML += '<br><a href="' + doc.url_document + '"><img src="' + url_image + '" width="400px" margin="0 auto"></a>';
+             } else {
+                codeHTML += '<br><a href="' + url_image + '"><img src="' + url_image + '" width="400px" margin="0 auto"></a>';
+             }
+          }
+          
+          if(doc.texte_explicatif.length>0){
+             codeHTML += '<br><small>'+doc.texte_explicatif+'</small>';
+          }
+          codeHTML = '<p class="subtitle-doc">' + codeHTML + '</p>';
+          
+          codeHTML = codeHTML.replace(/<a /gi, '<a target="_blank"');
+          auteur = auteur.replace(/<a /gi, '<a target="_blank"');
+          carddoc.push('<div class="doc" title="Document ajouté par ' + doc.contexte_ajout_ligne.replace("\"","") + '" onclick="ClicSurDoc(this)"><h3 class="title-doc"><span>&gt;</span><span><img src="assets/images/' + icon + '.png" alt=""></span> ' + auteurSansLien + doc.titre_document+'</h3><div class="hidden-doc">' + codeHTML + '</div></div>');
+        }
+        // Documents avec texte 
+        if(doc.type.toLowerCase() == "texte" || doc.type.toLowerCase() == "site web") {
+          let codeHTML = "";
+          let auteur = "";
+          let auteurSansLien = "";
+          codeHTML = nomAuteur(true, doc.auteur1_prenom, doc.auteur1_nom, doc.auteur1_url, doc.auteur2_prenom, doc.auteur2_nom, doc.auteur_autres);
+          auteurSansLien = nomAuteur(false, doc.auteur1_prenom, doc.auteur1_nom, doc.auteur1_url, doc.auteur2_prenom, doc.auteur2_nom, doc.auteur_autres);
+          if(codeHTML.length>0){
+             auteurSansLien += ", ";
+             codeHTML += ", ";
+          }
+          auteur = codeHTML;
+
+          if(doc.url_document.length>0){
+             if(doc.reference_bibliographique.length>0){
+                codeHTML += '<a href="' + doc.url_document + '">' + doc.reference_bibliographique + '</a>';
+             } else {
+                codeHTML += '<a href="' + doc.url_document + '">' + doc.titre_document + '</a>';
+             }
+          }else{
+             if(doc.reference_bibliographique.length>0){
+                codeHTML += doc.reference_bibliographique;
+             } else {
+                codeHTML += doc.titre_document;
+             }
+          }
+
+          let url_image = "";
+          if(doc.miniature.length > 0){
+             codeHTML += '<br><a href="' + doc.url_document + '"><img src="' + doc.miniature + '" width="400px" margin="0 auto"></a>';
+          }          
+
+          if(doc.texte_explicatif.length>0){
+             codeHTML += '<br><small>'+doc.texte_explicatif+'</small>';
+          }
+          codeHTML = '<p class="subtitle-doc">' + codeHTML + '</p>';
+          if(doc.texte.length>0){
+             codeHTML += '<p class="subtitle-doc">' + doc.texte + '</p>';
+          }
+          
+          codeHTML = codeHTML.replace(/<a /gi, '<a target="_blank"');
+          auteur = auteur.replace(/<a /gi, '<a target="_blank"');
+          carddoc.push('<div class="doc" title="Document ajouté par ' + doc.contexte_ajout_ligne.replace("\"","") + '" onclick="ClicSurDoc(this)"><h3 class="title-doc"><span>&gt;</span><span><img src="assets/images/texte.png" alt=""></span> ' + auteurSansLien + doc.titre_document+'</h3><div class="hidden-doc">' + codeHTML + '</div></div>');
+          
+        }
+        
+
+}
+
+// Fonction pour créer les marqueurs sur la carte et dans la timeline
+function createMarkers(dataintro, dataetape, datatexts, datamedia) {
+  // Sélection de la timeline
+  let e = document.querySelector('.timeline');
+  let card = document.querySelector(".overflow-cards");
+  let intro = document.querySelector(".introduction");
+  DocumentsParEtape(datatexts, DocParEtape);
+  DocumentsParEtape(datamedia, DocParEtape);
+  console.log("documents trouvés")
+  var latlngs = [];
+  // Itération pour chacune des villes dans le tableau data
+  for(i=0; i<dataetape.length;i++) {
+    if(dataetape[i].latitude !== '' && dataetape[i].longitude !== '') {
+
+    // Création des tableaux qui vont stocker les contenus
+    cardcontent = [];
+    carddoc = [];
+    test = [];
+    vignette = "assets/thumbnails/thumbnail-default.jpg";
+    
+    // Ajout des documents multimédia liés à l'étape
+    for(n=0; n<datamedia.length;n++) {
+      if(datamedia[n].id_etape == dataetape[i].id_etape) {
+        console.log("Document multimédia trouvé !")
+        // Si un document est une vignette, alors
+        if(datamedia[n].vignette == "1") {
+          // Push au début du tableau la ligne qu'il faudra mettre dans l'html
+          vignette = datamedia[n].miniature;
+          cardcontent.unshift('<div class="card-header"><img class="card-minia" src="'+vignette+'" alt""><div class="card-title"><h2>'+dataetape[i].lieu+'</h2><p>'+dataetape[i].date+'</p></div></div><div class="description"><p class="description-content">'+dataetape[i].description+'</p></div>');
+          // Création du pop up du marqueur sur la carte
+        }
+        else {
+          // Push dans le tableau une ligne par défaut
+          cardcontent.push('<div class="card-header"><img class="card-minia" src="'+vignette+'" alt""><div class="card-title"><h2>'+dataetape[i].lieu+'</h2><p>'+dataetape[i].date+'</p></div></div><div class="description"><p class="description-content">'+dataetape[i].description+'</p></div>');
+        }
+        addDocument(datamedia[n]);
+      }
+      else {
+        console.log("Document texte trouvé !")
+        // Push au début du tableau la ligne qu'il faudra mettre dans l'html
+        cardcontent.push('<div class="card-header"><img class="card-minia" src="'+vignette+'" alt""><div class="card-title"><h2>'+dataetape[i].lieu+'</h2><p>'+dataetape[i].date+'</p></div></div><div class="description"><p class="description-content">'+dataetape[i].description+'</p></div>');
+      }
+    }
+
+    // Ajout des documents texte liés à l'étape
+    for(n=0; n<datatexts.length;n++) {
+      if(datatexts[n].id_etape == dataetape[i].id_etape) {
+          // Push dans le tableau une ligne par défaut
+          cardcontent.push('<div class="card-header"><img class="card-minia" src="'+vignette+'" alt""><div class="card-title"><h2>'+dataetape[i].lieu+'</h2><p>'+dataetape[i].date+'</p></div></div><div class="description"><p class="description-content">'+dataetape[i].description+'</p></div>');
+          addDocument(datatexts[n]);
+      }
+      else {
+        // Push au début du tableau la ligne qu'il faudra mettre dans l'html
+        cardcontent.push('<div class="card-header"><img class="card-minia" src="'+vignette+'" alt""><div class="card-title"><h2>'+dataetape[i].lieu+'</h2><p>'+dataetape[i].date+'</p></div></div><div class="description"><p class="description-content">'+dataetape[i].description+'</p></div>');
+      }
+    }
+
+    carddoc = carddoc.toString().replace(/<\/p>\,/g,'</p>');
+    
+    let etapeLivre = "";
+    if((dataetape[i].page).length > 0){
+       etapeLivre = '<p class="text-click" onclick="onText(\''+dataetape[i].id_etape+'\')">Cette étape dans <u>'+dataintro[0].titre_texte+'</u></p>';
+    }
+    
+    // Insertion dans l'html des lignes définies plus tôt et du lien vers l'ouvrage
+    card.insertAdjacentHTML("beforeend", '<div id="card'+dataetape[i].id_etape+'"class="card">'+cardcontent[0]+etapeLivre+'<div class="card-content">'+(carddoc.toString().replace(/<\/div>\,/g,'</div>'))+'</div></div>');
+    // Insertion de chaque coordonnées dans le tableau latlngs
+    latlngs.push([dataetape[i].latitude, dataetape[i].longitude]);
+    }
+  };
+  
+  // Insertion dans l'html le texte et titre d'introduction
+  intro.insertAdjacentHTML("beforeend", '<div id="card" class="card-intro content"><h2 class="title-intro">'+dataintro[0].nom_itineraire+'</h2><p>'+dataintro[0].description_itineraire+'</p></div>')
+
+  // Création du About
+  let about = document.querySelector(".about");
+  about.insertAdjacentHTML("beforeend","<div class='card-about'><div class='content'><h2 class='title-about'>À propos</h2>"+dataintro[0].a_propos+"</div></div>");
+  
+  document.querySelector(".about").style.display="block";
+  document.querySelector(".card-about").style.display="block";
+  document.querySelector(".card-about .content").style.height="auto";
+  document.querySelector(".introduction").style.display="block";
+  document.querySelector(".card-intro").style.display="block";
+  document.querySelector(".overflow-cards").style.display="block";
+  document.querySelectorAll(".card").forEach(function(e){e.style.display = "block";});
+  document.querySelectorAll(".hidden-doc").forEach(function(e){e.classList.add("doc-open");});
+  
+};
+
+
+function DocumentsParEtape(docs, obj) {
+//console.log(docs);
+
+for(i=0;i<docs.length;i++) {
+  if(docs[i]["id_etape"] != ""){
+     if(docs[i]["id_etape"] in obj){
+        obj[docs[i]["id_etape"]] += 1;
+     } else {
+        obj[docs[i]["id_etape"]] = 1;
+     }
+  }
+  /*
+  if(docs[i].id_etape.length < 5) {
+    //obj += docs[i].id_etape;
+    //console.log(obj)
+  }
+  */
+}
+// var obj = {key1: "value1", key2: "value2"};
+// Object.assign(obj, {key3: "value3"});
+//console.log(obj)
+}
+
+// Clic pour voir le texte
+function onText(step) {
+  // Appartition du texte et disparition de l'étape
+  document.querySelector(".overflow-tl").classList.remove("-open");
+  document.querySelector(".overflow-cards").classList.remove("-open");
+  document.querySelector(".texte").classList.add("text-open1");
+  setTimeout(function (){
+    document.querySelector(".texte").classList.add("text-open2");
+  },100);
+  setTimeout(function() {
+    document.querySelector(".overflow-texte").classList.add("-open");
+    document.querySelector(".overflow-texte").scrollTo(0,document.querySelector("#s"+step).offsetTop-80)
+  },1100)
+
+  // Suite
+
+
+
+}
+
+// Animation lors de l'ouverture et fermeture d'un document
+function ClicSurDoc(e) {
+  if(!e.lastChild.classList.contains("doc-open")) {
+    let doc = document.querySelectorAll(".doc");
+    for (c=0;c<doc.length;c++) {
+      if(doc[c].lastChild.classList.contains('doc-open')) {
+        doc[c].lastChild.classList.remove('doc-open');
+        doc[c].firstChild.classList.remove('title-doc-clicked');
+        doc[c].firstChild.firstChild.classList.remove('rotate-span');
+      }
+    }
+    
+    setTimeout(function(){
+    let scrollitem = document.querySelector(".overflow-cards");
+    scrollitem.scrollTo({
+      top: (e.offsetTop - scrollitem.offsetTop),
+      behavior: 'smooth'
+    });
+    },500)
+    
+  }
+  e.lastChild.classList.toggle("doc-open");
+  e.firstChild.firstChild.classList.toggle("rotate-span");
+  e.firstChild.classList.toggle("title-doc-clicked");
+}
+
